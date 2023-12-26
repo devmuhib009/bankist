9f07be1b1b52f5ce89a817414f66dcdbbf5c1b0b
devmuhib009 <142894870+devmuhib009@users.noreply.github.com>
2023-12-26 12:59:11 +0600

    Navigation Hover End

--- script.js
+++ script.js
@@ -181,3 +181,26 @@ tabsContainer.addEventListener('click', e => {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+// Passing Arguments on Event
+
+// Menu fade animation
+
+const nav = document.querySelector('.nav');
+
+const handleHover = function (e) {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    console.log(link);
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
