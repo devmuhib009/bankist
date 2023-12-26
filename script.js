efd923459a6ff8a1a3119eff8af81d378546f644
devmuhib009 <142894870+devmuhib009@users.noreply.github.com>
2023-12-26 18:51:41 +0600

    New Scroll Method

--- script.js
+++ script.js
@@ -207,8 +207,23 @@ nav.addEventListener('mouseout', handleHover.bind(1));
 
 // Sticky Navigation
 
-const initialCoords = section1.getBoundingClientRect();
+// const initialCoords = section1.getBoundingClientRect();
 
-window.addEventListener('scroll', function () {
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
-});
+// window.addEventListener('scroll', function () {
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
+// });
+
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// };
+
+// const obsOptions = {
+//   root: null,
+//   treshold: 0.1,
+// };
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
