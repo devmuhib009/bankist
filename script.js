6785dafed8be3bf5222ad446254a818b4cc64eb9
devmuhib009 <142894870+devmuhib009@users.noreply.github.com>
2023-12-26 13:17:00 +0600

    Adding Sticky Class

--- script.js
+++ script.js
@@ -204,3 +204,11 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky Navigation
+
+const initialCoords = section1.getBoundingClientRect();
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+});
