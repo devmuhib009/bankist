622c2462713be038b269b18f85e0785bb6884df2
devmuhib009 <142894870+devmuhib009@users.noreply.github.com>
2023-12-26 11:10:53 +0600

    Dom Triversing

--- script.js
+++ script.js
@@ -135,3 +135,26 @@ btnScrollTo.addEventListener('click', function (e) {
 //     this.style.backgroundColor = randomColor();
 //     console.log('NAV', e.target, e.currentTarget);
 //   });
+
+const h1 = document.querySelector('h1');
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+// h1.closest('h1').style.background = 'var(--color-primary)';
+
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+console.log(h1.parentElement.children);
+[...h1.parentElement.children].forEach(function (e) {
+  if (e !== h1) {
+    e.style.background = 'green';
+  }
+});
